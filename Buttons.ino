b4b7bdaa0b52357c523102af11560220921df826
AssaahhDudeImChad <85378098+AssaahhDudeImChad@users.noreply.github.com>
2021-09-04 20:23:00 +1200

    Buttons

---
+++ Buttons.ino
@@ -0,0 +1,20 @@
+int Button1 = 8;
+Val1 = 0;
+
+void setup(){
+  pinMode(Button1, INPUT_PULLUP);
+  
+}
+
+void loop(){
+  
+}
+
+int isPressed(){
+  Val1 = digtialRead(8);
+  
+  if(Val1 == HIGH){
+    
+  }
+  
+}
